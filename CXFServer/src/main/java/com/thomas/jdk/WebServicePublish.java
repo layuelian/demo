e1e853277c926ff2e4cd1e1a7ebe6e55df77c347
layuelian <layuelian534@163.com>
2018-11-05 14:48:12 +0800

    jdk和spring cxf REST方式两种方式实现WebService

--- CXFServer/src/main/java/com/thomas/jdk/WebServicePublish.java
+++ CXFServer/src/main/java/com/thomas/jdk/WebServicePublish.java
@@ -3,14 +3,14 @@ package com.thomas.jdk;
 import javax.xml.ws.Endpoint;
 
 public class WebServicePublish {
-    public static void main(String[] args) {
-        //定义WebService发布地址，这个地址就是提供给外界访问的WebService
-        //的URL地址。格式：http://ip:端口号/xxxx
-
-        //访问时需在路径后加?wsdl
-
-        String address="http://127.0.0.1:9000/Webservice";
-        Endpoint.publish(address,new WebServiceImpl());
-        System.out.println("WebService发布成功!");
-    }
+//    public static void main(String[] args) {
+//        //定义WebService发布地址，这个地址就是提供给外界访问的WebService
+//        //的URL地址。格式：http://ip:端口号/xxxx
+//
+//        //访问时需在路径后加?wsdl
+//
+//        String address="http://127.0.0.1:9000/Webservice";
+//        Endpoint.publish(address,new WebServiceImpl());
+//        System.out.println("WebService发布成功!");
+//    }
 }
